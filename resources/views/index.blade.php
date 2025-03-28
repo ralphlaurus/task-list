3d1c1647fa5dd15cd80bc71d42e750ad32f0eb39
Raphael Jairos <47027274+jairos75@users.noreply.github.com>
2025-03-28 14:36:01 +0300

    Initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('title', 'The list of tasks')
+
+
+@section('content')
+{{--    @if (count($tasks))--}}
+{{--        @foreach($tasks as $task)--}}
+{{--            <div>{{ $task->title  }}</div>--}}
+{{--        @endforeach--}}
+{{--    @else--}}
+{{--        <div>There are no tasks!</div>--}}
+{{--    @endif--}}
+    @forelse ($tasks as $task)
+        <div>
+            <a href="{{ route('tasks.show', ['id' => $task->id]) }}">
+                {{ $task->title }}
+            </a>
+        </div>
+    @empty
+        <div>There are no tasks</div>
+    @endforelse
+@endsection
