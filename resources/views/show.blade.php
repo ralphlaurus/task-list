3cc46f517fca2f3cea7a7ba3d761dfa86207f0db
Raphael Jairos <47027274+jairos75@users.noreply.github.com>
2025-04-02 16:47:33 +0300

    deleting data

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -6,10 +6,18 @@
 @section('content')
     <p>{{ $task->description }}</p>
 
-    @if($task->long_description)
+    @if ($task->long_description)
         <p>{{ $task->long_description }}</p>
     @endif
 
     <p>{{ $task->created_at }}</p>
     <p>{{ $task->updated_at }}</p>
+
+    <div>
+        <form method="POST" action="{{ route('tasks.destroy', ['task' => $task->id]) }}">
+            @csrf
+            @method('DELETE')
+            <button type="submit">Delete</button>
+        </form>
+    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::get('/tasks', function () {
     return view('index', [
         'tasks' => Task::latest()->get(),
     ]);
-});
+})->name('tasks.index');
 
 Route::view('/tasks/create', 'create')
     ->name('tasks.create');
@@ -32,12 +32,6 @@ Route::get('tasks/{task}', function (Task $task) {
 })->name('tasks.show');
 
 Route::post('/tasks', function (TaskRequest $request) {
-    // $data = $request->validated();
-    // $task = new Task;
-    // $task->title = $data['title'];
-    // $task->description = $data['description'];
-    // $task->long_description = $data['long_description'];
-    // $task->save();
     $task = Task::create($request->validated());
 
     return redirect()->route('tasks.show', ['task' => $task->id])
@@ -45,16 +39,18 @@ Route::post('/tasks', function (TaskRequest $request) {
 })->name('tasks.store');
 
 Route::put('/tasks/{task}', function (Task $task, TaskRequest $request) {
-    // $data = $request->validated();
-    // $task->title = $data['title'];
-    // $task->description = $data['description'];
-    // $task->long_description = $data['long_description'];
-    // $task->save();
-
     $task->update($request->validated());
+
     return redirect()->route('tasks.show', ['task' => $task->id])
         ->with('success', 'Task updated successfully!');
 })->name('tasks.update');
+
+Route::delete('/tasks/{task}', function (Task $task) {
+    $task->delete();
+
+    return redirect()->route('tasks.index')
+        ->with('success', 'Task deleted successfully!');
+})->name('tasks.destroy');
 
 Route::fallback(function () {
     return 'Still got somewhere!';
