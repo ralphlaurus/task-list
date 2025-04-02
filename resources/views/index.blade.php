c2d192785e1518e14f2ac66e29e2bf848c0fc212
Raphael Jairos <47027274+jairos75@users.noreply.github.com>
2025-04-02 16:10:07 +0300

    reusability(route model binding form requess, mass assignment)

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,16 +4,16 @@
 
 
 @section('content')
-{{--    @if (count($tasks))--}}
-{{--        @foreach($tasks as $task)--}}
-{{--            <div>{{ $task->title  }}</div>--}}
-{{--        @endforeach--}}
-{{--    @else--}}
-{{--        <div>There are no tasks!</div>--}}
-{{--    @endif--}}
+    {{--    @if (count($tasks)) --}}
+    {{--        @foreach ($tasks as $task) --}}
+    {{--            <div>{{ $task->title  }}</div> --}}
+    {{--        @endforeach --}}
+    {{--    @else --}}
+    {{--        <div>There are no tasks!</div> --}}
+    {{--    @endif --}}
     @forelse ($tasks as $task)
         <div>
-            <a href="{{ route('tasks.show', ['id' => $task->id]) }}">
+            <a href="{{ route('tasks.show', ['task' => $task->id]) }}">
                 {{ $task->title }}
             </a>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Task;
 use Illuminate\Http\Request;
+use App\Http\Requests\TaskRequest;
 use Illuminate\Support\Facades\Route;
 
 
@@ -18,49 +19,40 @@ Route::get('/tasks', function () {
 Route::view('/tasks/create', 'create')
     ->name('tasks.create');
 
-Route::get('tasks/{id}/edit', function ($id) {
+Route::get('tasks/{task}/edit', function (Task $task) {
     return view('edit', [
-        'task' => Task::findOrFail($id),
+        'task' => $task,
     ]);
 })->name('tasks.edit');
 
-Route::get('tasks/{id}', function ($id) {
+Route::get('tasks/{task}', function (Task $task) {
     return view('show', [
-        'task' => Task::findOrFail($id),
+        'task' => $task,
     ]);
 })->name('tasks.show');
 
-Route::post('/tasks', function (Request $request) {
-    $data = $request->validate([
-        'title' => 'required|max:255',
-        'description' => 'required',
-        'long_description' => 'required',
-    ]);
+Route::post('/tasks', function (TaskRequest $request) {
+    // $data = $request->validated();
+    // $task = new Task;
+    // $task->title = $data['title'];
+    // $task->description = $data['description'];
+    // $task->long_description = $data['long_description'];
+    // $task->save();
+    $task = Task::create($request->validated());
 
-    $task = new Task;
-    $task->title = $data['title'];
-    $task->description = $data['description'];
-    $task->long_description = $data['long_description'];
-    $task->save();
-
-    return redirect()->route('tasks.show', ['id' => $task->id])
+    return redirect()->route('tasks.show', ['task' => $task->id])
         ->with('success', 'Task created successfully!');
 })->name('tasks.store');
 
-Route::put('/tasks/{id}', function ($id, Request $request) {
-    $data = $request->validate([
-        'title' => 'required|max:255',
-        'description' => 'required',
-        'long_description' => 'required',
-    ]);
+Route::put('/tasks/{task}', function (Task $task, TaskRequest $request) {
+    // $data = $request->validated();
+    // $task->title = $data['title'];
+    // $task->description = $data['description'];
+    // $task->long_description = $data['long_description'];
+    // $task->save();
 
-    $task = Task::findOrFail($id);
-    $task->title = $data['title'];
-    $task->description = $data['description'];
-    $task->long_description = $data['long_description'];
-    $task->save();
-
-    return redirect()->route('tasks.show', ['id' => $task->id])
+    $task->update($request->validated());
+    return redirect()->route('tasks.show', ['task' => $task->id])
         ->with('success', 'Task updated successfully!');
 })->name('tasks.update');
 
