bd09c816eb3104322b32c6beef759dac8a30be4d
Raphael Jairos <47027274+jairos75@users.noreply.github.com>
2025-04-03 07:48:26 +0300

    adding pagination

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,13 +4,6 @@
 
 
 @section('content')
-    {{--    @if (count($tasks)) --}}
-    {{--        @foreach ($tasks as $task) --}}
-    {{--            <div>{{ $task->title  }}</div> --}}
-    {{--        @endforeach --}}
-    {{--    @else --}}
-    {{--        <div>There are no tasks!</div> --}}
-    {{--    @endif --}}
     @forelse ($tasks as $task)
         <div>
             <a href="{{ route('tasks.show', ['task' => $task->id]) }}">
@@ -20,4 +13,9 @@
     @empty
         <div>There are no tasks</div>
     @endforelse
+    @if ($tasks->count())
+        <nav>
+            {{ $tasks->links() }}
+        </nav>
+    @endif
 @endsection
