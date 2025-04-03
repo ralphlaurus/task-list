a59117b0cfc983734e41da5b7169bc81650cf838
Raphael Jairos <47027274+jairos75@users.noreply.github.com>
2025-04-03 08:30:46 +0300

    toggling task state

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,9 @@
 
 
 @section('content')
+    <div>
+        <a href="{{ route('tasks.create') }}">Add Task!</a>
+    </div>
     @forelse ($tasks as $task)
         <div>
             <a href="{{ route('tasks.show', ['task' => $task->id]) }}">
